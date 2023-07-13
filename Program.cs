3c500f2c2483ebf0a393415849a393e7e40bbccf
lavanyaboddu31@gmail.com <lavanyaboddu31@gmail.com>
2023-07-13 15:07:02 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5_dat4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "lavanya";
+            string password = "password";
+            int attempts = 0;
+
+            while (attempts < 3)
+            {
+                Console.Write("Enter username: ");
+                string inputUsername = Console.ReadLine();
+
+                Console.Write("Enter password: ");
+                string inputPassword = Console.ReadLine();
+
+                if (inputUsername == username && inputPassword == password)
+                {
+                    Console.WriteLine("Login successful.");
+                    break;
+                }
+                else if (inputUsername == username && inputPassword != password)
+                {
+                    Console.WriteLine("Invalid password.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username.");
+                }
+
+                attempts++;
+            }
+
+            if (attempts == 3)
+            {
+                Console.WriteLine("Login failed.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
